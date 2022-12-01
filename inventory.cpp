4bf19f35f1f7bbe26a28e33fa95e48637b0d687c
René Abraham Calzadilla Calderón <119465964+rene-calz@users.noreply.github.com>
2022-11-30 19:48:11 -0600

    Update inventory.cpp
    Trabajo completa. Hay bugs a la hora de ingresar la dirección (relacionado con los espacios, probablemente). Y hay que añadir un mensaje que indique que no hay información coincidente cuando no la hay.

--- inventory.cpp
+++ inventory.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-
+#include <stdlib.h>
 using namespace std; 
 				
 class Pymes{
-	private: 
+	public: 
 		string namePyme; 
 		string rfcPyme; 
 		string adressPyme;
@@ -68,12 +68,56 @@ class Pymes{
 			cout << "Direccion: " << adressPyme << endl;
 			cout << "Nombre del director: " << ceoPyme << endl; 
 			cout << "Numero de trabajadores: " << numWorkers << endl; 
-			cout << "Cantidad de pesos por ano: " << moneyYear << endl;
+			cout << "Cantidad de pesos por anio: " << moneyYear << endl;
+		}
+};
+
+
+class Worker: public Pymes{
+	private: 
+		string name;
+		int age; 
+		string position;
+		
+	public: 
+		void setNameWorker(string s){
+			name = s;
+		}
+		
+		void setAge(int a){
+			age = a;
+		}
+		
+		void setPosition(string p){
+			position = p;
+		}
+		
+		string getNameWorker(){
+			return name;
+		}
+		
+		int getAge(){
+			return  age;
+		}
+		
+		string getPosition(){
+			return position;
+		}
+		
+		void showWorker(){
+			cout << "Informacion disponible" << endl; 
+			cout << "Nombre: " << name << endl; 
+			cout << "Edad: " << age << endl; 
+			cout << "Posicion: " << position << endl;
+			cout << "Pyme donde trabaja: " << namePyme << endl; 
+			cout << "RFC de Pyme: " << rfcPyme << endl; 
+			cout << "Dirección donde trabaja: " << adressPyme << endl;
 		}
 };
 
 
 Pymes pmes[1000];
+Worker wrkr[1000];
 
 void addPyme(int i){
 	string name, rfc, adress, ceo; 
@@ -86,7 +130,7 @@ void addPyme(int i){
 	cout << "Ingrese la direccion: " << endl; cin >> adress; 
 	cout << "Ingrese el nombre del director: " << endl; cin >> ceo; 
 	cout << "Ingrese la cantidad de empleados: " << endl; cin >> num;
-	cout << "Ingrese la cantidad de pesos por ano: " <<endl ; cin >> money; 
+	cout << "Ingrese la cantidad de pesos por anio: " <<endl ; cin >> money; 
 	
 	pmes[k].setName(name);
 	pmes[k].setRFC(rfc);
@@ -95,6 +139,31 @@ void addPyme(int i){
 	pmes[k].setNum(num);
 	pmes[k].setMoney(money);
 	
+	system("CLS");
+	
+}
+
+void addWorker(int i){
+	string name, position, pyName, pyRFC, pyAdress; 
+	int age, k = i; 
+	
+	cout << "Agregar un trabajador " << endl; 
+	cout << "Ingrese el nombre: " << endl; cin >> name; 
+	cout << "Ingrese la edad: " << endl; cin >> age; 
+	cout << "Ingrese la posicion: " << endl; cin >> position; 
+	cout << "Ingrese el nombre de la empresa donde trabaja: " << endl; cin >> pyName; 
+	cout << "Ingrese la RFC de donde trabaja: " << endl; cin >> pyRFC;
+	cout << "Ingrese la direccion de la empresa donde trabaja: " <<endl ; cin >> pyAdress; 
+	
+	wrkr[k].setNameWorker(name);
+	wrkr[k].setAge(age);
+	wrkr[k].setPosition(position);
+	wrkr[k].setName(pyName);
+	wrkr[k].setRFC(pyRFC);
+	wrkr[k].setAdress(pyAdress);
+	
+	system("CLS");
+	
 }
 	
 void lookPyme(int i){
@@ -102,8 +171,9 @@ void lookPyme(int i){
 		string name, rfc, ceo, adress;
 		float money; 
 		
-		cout << "Ingrese el criterio de busqueda: " << endl << "(1) Por nombre" << endl << "(2) Por RFC" << endl <<  "(3) Por direccion " << endl << "(4) Por director" << endl << "(5) Por numero de empleados" << endl << "(6) Por cantidad de pesos al ano" << endl; 
+		cout << "Ingrese el criterio de busqueda: " << endl << "(1) Por nombre" << endl << "(2) Por RFC" << endl <<  "(3) Por direccion " << endl << "(4) Por director" << endl << "(5) Por numero de empleados" << endl << "(6) Por cantidad de pesos al anio" << endl; 
 		cin >> d; 
+		system("CLS");
 		
 		switch(d){
 			case 1: cout << "Ingrese el nombre por buscar: " << endl; cin >> name;
@@ -114,6 +184,7 @@ void lookPyme(int i){
 							cout << endl; 
 						} 
 					}
+					system ("PAUSE");
 					break; 
 			
 			case 2: cout << "Ingrese el RFC por buscar: " << endl; cin >> rfc;
@@ -124,6 +195,7 @@ void lookPyme(int i){
 							cout << endl; 
 						} 
 					}
+					system ("PAUSE");
 					break; 
 					
 			case 3: cout << "Ingrese la direccion por buscar: " << endl; cin >> adress;
@@ -134,6 +206,7 @@ void lookPyme(int i){
 							cout << endl; 
 						} 
 					}
+					system ("PAUSE");
 					break; 
 					
 			case 4: cout << "Ingrese el nombre de director por buscar: " << endl; cin >> ceo;
@@ -144,6 +217,7 @@ void lookPyme(int i){
 							cout << endl; 
 						} 
 					}
+					system ("PAUSE");
 					break; 
 					
 			case 5: cout << "Ingrese el numero de empleados por buscar: " << endl; cin >> numWorkers;
@@ -154,9 +228,10 @@ void lookPyme(int i){
 							cout << endl; 
 						} 
 					}
+					system ("PAUSE");
 					break; 
 					 
-			case 6: cout << "Ingrese la cantidad de pesos por ano por buscar: " << endl; cin >> money;
+			case 6: cout << "Ingrese la cantidad de pesos por anio por buscar: " << endl; cin >> money;
 					for (int i = 0; i < k; i++){
 						if (pmes[i].getMoneyYear() == money){
 							cout << "Pyme " << i + 1 << " con informacion coincidente: " << endl; 
@@ -164,6 +239,77 @@ void lookPyme(int i){
 							cout << endl; 
 						} 
 					}
+					system ("PAUSE");
+					break; 
+			
+			default: cout << "Opcion invalida, intente de nuevo..." << endl; break;
+		}
+	
+}
+
+void lookWorker(int i){
+		int k = i+1, d, numWorkers, age;
+		string name, rfc, ceo, adress, position, company;
+		float money; 
+		
+		cout << "Ingrese el criterio de busqueda: " << endl << "(1) Por nombre" << endl << "(2) Por edad" << endl <<  "(3) Por posicion " << endl << "(4) Por empresa" << endl << "(5) Por RFC de la empresa" << endl;
+		cin >> d;
+		system("CLS");
+		
+		switch(d){
+			case 1: cout << "Ingrese el nombre por buscar: " << endl; cin >> name;
+					for (int i = 0; i < k; i++){
+						if (wrkr[i].getNameWorker() == name){
+							cout << "Trabajador " << i + 1 << " con informacion coincidente: " << endl; 
+							wrkr[i].showWorker();
+							cout << endl; 
+						} 
+					}
+					system ("PAUSE");
+					break; 
+			
+			case 2: cout << "Ingrese edad por buscar: " << endl; cin >> age;
+					for (int i = 0; i < k; i++){
+						if (wrkr[i].getAge() == age){
+							cout << "Trabajador " << i + 1 << " con informacion coincidente: " << endl; 
+							wrkr[i].showWorker();
+							cout << endl; 
+						} 
+					}
+					system ("PAUSE");
+					break; 
+					
+			case 3: cout << "Ingrese la posicion por buscar: " << endl; cin >> position;
+					for (int i = 0; i < k; i++){
+						if (wrkr[i].getPosition() == position){
+							cout << "Trabajador " << i + 1 << " con informacion coincidente: " << endl; 
+							wrkr[i].showWorker();
+							cout << endl; 
+						} 
+					}
+					system ("PAUSE");
+					break; 
+					
+			case 4: cout << "Ingrese la empresa por buscar: " << endl; cin >> company;
+					for (int i = 0; i < k; i++){
+						if (pmes[i].getName() == company){
+							cout << "Trabajador " << i + 1 << " con informacion coincidente: " << endl; 
+							pmes[i].showPyme();
+							cout << endl; 
+						} 
+					}
+					system ("PAUSE");
+					break; 
+					
+			case 5: cout << "Ingrese el RFC por buscar: " << endl; cin >> rfc;
+					for (int i = 0; i < k; i++){
+						if (wrkr[i].getRFC() == rfc){
+							cout << "Trabajador " << i + 1 << " con informacion coincidente: " << endl; 
+							wrkr[i].showWorker();
+							cout << endl; 
+						} 
+					}
+					system ("PAUSE");
 					break; 
 			
 			default: cout << "Opcion invalida, intente de nuevo..." << endl; break;
@@ -173,7 +319,7 @@ void lookPyme(int i){
 
 
 int main(){
-	int d, o = -1;
+	int d, py = -1, wk = -1;
 	bool flag = true;
 
 	
@@ -184,19 +330,17 @@ int main(){
 		cout << "(1) Agregar PYME" << endl << "(2) Agregar trabajador" << endl << "(3) Consultar PYME" << endl << "(4) Consultar trabajador" << endl << "(5) Cerrar" << endl; cin >> d;
 		
 		switch(d){
-			case 1: o++; addPyme(o); break; 
+			case 1: py++; system("CLS"); addPyme(py); break; 
 			
-			//case 2: ; break; 
+			case 2: wk++; system("CLS"); addWorker(wk); break; 
 			
-			case 3: lookPyme(o); break;
+			case 3: system("CLS"); lookPyme(py); break;
 			
-			//case 4: mayor(); break;
+			case 4: system("CLS"); lookWorker(wk); break;
 			
-			//case 5: frase(); break;
+			case 5: system("CLS"); cout << endl << "How did we get this far apart? " << endl << "We used to be so close together" << endl << "How did we get this far apart?" << endl << "I thought this love would last forever" << endl << "- Apart by The Cure" << endl << endl << "Bye..."; flag = false; break; 
 			
-			case 5: flag = false; break; 
-			
-			default: cout << "Opcion invalida, intente de nuevo. " << endl;
+			default: system("CLS"); cout << "Opcion invalida, intente de nuevo. " << endl;
 		}
 	
 	}
